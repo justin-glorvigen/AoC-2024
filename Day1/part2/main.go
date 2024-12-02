98b6d724447f7bb67c8a5419aa2f38f3252d1f91
Justin Glorvigen <justin.glorvigen@paramount.com>
2024-12-02 11:25:42 -0700

    Day1

---
+++ Day1/part2/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	filename := "input.txt"
+
+	contents, _ := os.ReadFile(filename)
+
+	strContents := string(contents)
+
+	left, right := []int{}, []int{}
+
+	lines := strings.Split(strContents, "\n")
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		items := strings.Split(line, "   ")
+		if len(items) != 2 {
+			fmt.Println(fmt.Sprintf("Error parsing line %s", line))
+		}
+
+		leftItem, err := strconv.Atoi(items[0])
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Error converting left %s to int, err=%v", items[0], err))
+		} else {
+			left = append(left, leftItem)
+		}
+		rightItem, err := strconv.Atoi(items[1])
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Error converting right %s to int, err=%v", items[1], err))
+		} else {
+			right = append(right, rightItem)
+		}
+	}
+
+	// similarityScores := map[int]int{}
+	similaritySum := int64(0)
+	for i := 0; i < len(left); i++ {
+		findCount := 0
+		for j := 0; j < len(right); j++ {
+			if right[j] == left[i] {
+				findCount = findCount + 1
+			}
+		}
+
+		// similarityScores[left[i]] = findCount
+		similaritySum += int64(left[i]) * int64(findCount)
+	}
+
+	fmt.Println(similaritySum)
+}
